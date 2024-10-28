d2682e9d03a188189e0241f45f9f4619e37f0b9a
Seong Yeol Hwang <seongyeol_hwang@tmax.co.kr>
2024-10-28 10:15:27 +0900

    [Fix] Redirect 추가
    - 첫 진입시 바로 all-users로 이동

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  redirects: async () => {
+    return [
+      {
+        source: "/",
+        destination: "/all-users",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
